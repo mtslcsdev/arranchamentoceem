28ca3152cf7272b8b9663cbbe8182630cfbf1b96
Mateus Lucas <155203517+mtslcsdev@users.noreply.github.com>
2024-02-03 09:48:55 -0300

    arranchamento

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector('.box form');
+    var thankYouContainer = document.getElementById('thank-you-container');
+    var boxContainer = document.querySelector('.box');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+
+        // Aqui você pode adicionar a lógica para processar o formulário, se necessário
+
+        // Oculta a div .box
+        boxContainer.style.display = 'none';
+
+        // Exibe a mensagem de agradecimento
+        thankYouContainer.style.display = 'block';
+    });
+});
